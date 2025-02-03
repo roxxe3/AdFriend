b0900f2ecc75356e2ee58bbd5bfa08260e295a4b
roxxe3 <hamzafarissi@gmail.com>
2025-02-03 20:43:05 +0100

    Add click event handling to display message in popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,7 @@
 // This file is intentionally left blank.
+const container = document.getElementById("popup-container");
 document.getElementById("action-button").addEventListener("click", () => {
     console.log('Extension icon clicked');
+    container.innerHTML += "<p>Extension icon clicked</p>";
     
 });
